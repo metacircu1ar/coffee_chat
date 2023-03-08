e32d19835fa97056e79e63c0654fbf3348c8d503
Max <maxtikhomirov@outlook.com>
2023-03-09 01:56:57 +0400

    Added indexing by user_id to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_131217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "unread_counts", force: :cascade do |t|
